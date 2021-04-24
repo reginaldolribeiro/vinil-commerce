cc67c997282e2402c3025bd068d240a462769c3c
Reginaldo Ribeiro <reginaldolribeiro@gmail.com>
2021-04-23 22:56:07 -0300

    add createdAt/updatedAt fields

--- src/main/kotlin/com/vinilcommerce/vinilcommerce/model/Sale.kt
+++ src/main/kotlin/com/vinilcommerce/vinilcommerce/model/Sale.kt
@@ -2,6 +2,7 @@ package com.vinilcommerce.vinilcommerce.model
 
 import com.fasterxml.jackson.annotation.JsonManagedReference
 import java.math.BigDecimal
+import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
@@ -16,11 +17,15 @@ data class Sale(
 
     @JsonManagedReference
     @OneToMany(mappedBy = "sale", cascade = [CascadeType.ALL])
-    var items: List<ItemSale> = emptyList()
+    var items: List<ItemSale> = emptyList(),
+    @Column(name = "created_at", updatable = false, nullable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    @Column(name = "updated_at", updatable = true)
+    val updatedAt: LocalDateTime? = null
 ) {
     fun calculateTotalCashback() = items.map { item -> item.cashbackValue }.reduce { acc, next -> acc.plus(next) }
     fun calculateTotalValue() = items.map { item -> item.price }.reduce { acc, next -> acc.plus(next) }
-    fun calculate(){
+    fun calculate() {
         items.forEach { item ->
             this.totalCashback = this.totalCashback.add(item.cashbackValue)
             this.totalValue = this.totalValue.add(item.price.plus())
